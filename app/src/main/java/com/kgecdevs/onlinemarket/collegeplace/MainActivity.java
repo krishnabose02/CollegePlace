308ceebf0cfe5e0a5c5677238cbb5779204deeb5
krishnabose02 <krishna.bose02@gmail.com>
2017-11-15 23:01:54 +0530

    Initial commit

---
+++ app/src/main/java/com/kgecdevs/onlinemarket/collegeplace/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kgecdevs.onlinemarket.collegeplace;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+int c=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void goAdd(View view) {
+
+    }
+}
